abc2b21cb1ececf9f8cab53dbd6e2c87244bc210
QuiVo <quivv.dtp@gmail.com>
2020-12-24 21:06:26 +0700

    Update-Database

--- eShopSolution.Data/Entities/Product.cs
+++ eShopSolution.Data/Entities/Product.cs
@@ -6,7 +6,7 @@ namespace eShopSolution.Data.Entities
 {
     public class Product
     {
-        public string Id { set; get; }
+        public int Id { set; get; }
         public decimal Price { set; get; }
         public decimal OriginalPrice { set; get; }
         public int Stock { set; get; }
